d77be8478348e1d1397993895fd37caa9fbf2e22
withkittens <withkittens@users.noreply.github.com>
2019-12-14 02:27:15 +0300

    Update 'actions' branch with latest changes in 'master' (#1)

---
+++ samples/NCurly.Samples/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NCurly.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var curly = new CurlyClient();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
